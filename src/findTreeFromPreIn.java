a35bed04a7f4b50c521f69ed101b7299a4ce1cfd
jommersu <jommer.su@foxmail.com>
2018-03-31 13:55:21 +0800

    2018/03/31, findTreeBy Pre and in, getMin from rotateArray

---
+++ src/findTreeFromPreIn.java
@@ -0,0 +1,59 @@
+/*
+重建二叉树
+ */
+
+/*
+输入某二叉树的前序遍历和中序遍历的结果，请重建出该二叉树。
+假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
+例如输入前序遍历序列{1,2,4,7,3,5,6,8}和中序遍历序列{4,7,2,1,5,3,8,6}，
+则重建二叉树并返回。
+
+ */
+public class findTreeFromPreIn {
+
+    /*
+     Definition for binary tree
+     */
+    static class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+      }
+
+      public static void main(String[] args){
+         int[] pre = {1,2,3,4,5,6,7};
+         int[] in = {3,2,4,1,6,5,7};
+         System.out.println(reConstructBinaryTree(pre,in).val);
+      }
+
+
+    public static TreeNode reConstructBinaryTree(int[] pre, int[] in) {
+        if (pre == null || in == null) {
+            return null;
+        }
+
+        int left = 0;
+        int right = in.length - 1;
+        int index = 0;
+        return findTreePreIn(pre, in, left, right,index);
+
+    }
+
+    public static TreeNode findTreePreIn(int[] pre, int[] in, int left, int right, int index) {
+          if(left >right){
+              return null;
+          }
+        int inHead = 0;
+        TreeNode head = new TreeNode(pre[index]);
+        for (int i = left; i <= right; i++) {
+            if (head.val == in[i]) {
+                inHead = i;
+                break;
+            }
+        }
+        head.left = findTreePreIn(pre, in, left , inHead - 1,index + 1);
+        head.right = findTreePreIn(pre, in, inHead + 1, right, index + inHead- left + 1);
+        return head;
+    }
+}
---
+++ src/rotateArray.java
@@ -0,0 +1,61 @@
+/*
+旋转数组的最小数字
+ */
+/*
+把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。
+输入一个非递减排序的数组的一个旋转，输出旋转数组的最小元素。
+例如数组{3,4,5,1,2}为{1,2,3,4,5}的一个旋转，该数组的最小值为1。
+NOTE：给出的所有元素都大于0，若数组大小为0，请返回0。
+ */
+
+public class rotateArray {
+    public static void main(String[] args){
+        int[] array = {3,4,5,1,2};
+
+        System.out.println(getMin(array));
+    }
+    public static int getMin(int [] array) {
+        if(array.length == 0){
+            return 0;
+        }
+        if(array.length == 1){
+            return array[0];
+        }
+        int left = 0;
+        int right = array.length - 1;
+        int mid = 0;
+
+        while(left < right){
+            //not rotate
+
+            if(array[left] < array[right]){
+                return array[0];
+            }
+
+            if(left == right - 1){
+                return array[left] <= array[right] ? array[left] : array[right];
+            }
+            mid = (left + right) / 2;
+
+            if(array[left] > array[mid]){
+                right = mid;
+                continue;
+            }
+
+            if(array[mid] > array[right]){
+                left = mid;
+                continue;
+            }
+
+            if(array[left] == array[mid] && array[mid] == array[right]){
+                int min = 0;
+                for(int i = 0; i < array.length; i++){
+                    min = array[i] <= min ? array[i] : min;
+                }
+                return min;
+            }
+
+        }
+        return Math.min(array[left],array[right]);
+    }
+}
