ea7d2aef259c6e7fe02db2e29f39e621c7de23eb
jommersu <jommer.su@foxmail.com>
2018-06-29 10:37:39 +0800

    字符串中替换空格

--- src/replaceSpace.java
+++ src/replaceSpace.java
@@ -1,10 +1,12 @@
 /*
-字符串
+字符串	替换空格
  */
 /*
 请实现一个函数，
 将一个字符串中的空格替换成“%20”。
 例如，当字符串为We Are Happy.则经过替换之后的字符串为We%20Are%20Happy。
+
+利用Java语言的特性，StringBuffer的方法：append
  */
 
 public class replaceSpace {
